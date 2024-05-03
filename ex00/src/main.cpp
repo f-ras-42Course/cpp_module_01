d082b2043d9c0be91d82743785ac2cb4ddd115a9
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2024-05-03 21:15:27 +0200

    new files

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,31 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   megaphone.cpp                                      :+:    :+:            */
+/*   main.cpp                                           :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/03 16:52:55 by fras          #+#    #+#                 */
-/*   Updated: 2024/05/03 21:00:34 by fras          ########   odam.nl         */
+/*   Updated: 2024/05/03 21:14:44 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-
-int	main(int argc, char *argv[])
-{
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	else
-	{
-		for (int i = 1; i < argc; i++)
-		{
-			for (char *c = argv[i]; *c != '\0'; c++)
-			{
-				std::putchar(std::toupper(*c));
-			}
-		}
-	}
-	std::cout << '\n';
-	return EXIT_SUCCESS;
-}
