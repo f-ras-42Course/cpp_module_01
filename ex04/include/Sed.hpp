4e08213314e8776ef9ab3900730dd2e09522c4d1
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2024-05-11 23:16:36 +0200

    moved inputfile more local

--- ex04/include/Sed.hpp
+++ ex04/include/Sed.hpp
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/09 10:20:22 by fras          #+#    #+#                 */
-/*   Updated: 2024/05/11 23:04:35 by fras          ########   odam.nl         */
+/*   Updated: 2024/05/11 23:13:38 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@
 class Sed
 {
 	private:
-		std::ifstream	inputfile_;
 		std::string		filecontents_;
 	public:
 		Sed(const std::string& filename);
--- ex04/src/Sed.cpp
+++ ex04/src/Sed.cpp
@@ -6,16 +6,16 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/09 10:20:26 by fras          #+#    #+#                 */
-/*   Updated: 2024/05/11 23:04:29 by fras          ########   odam.nl         */
+/*   Updated: 2024/05/11 23:13:59 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Sed.hpp"
 
 Sed::Sed(const std::string& filename)
-: inputfile_(filename)
 {
-	std::stringstream inputbuffer;
+	std::ifstream		inputfile_(filename);
+	std::stringstream	inputbuffer;
 	
 	std::cout << "Constructor called for " << filename << "! \n";
 	if (!inputfile_.is_open())
