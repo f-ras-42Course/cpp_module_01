7629b09dd64e40065dd3ed84867e597b239e3c5e
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2024-05-12 08:59:14 +0200

    removed string header from main

--- ex04/src/main.cpp
+++ ex04/src/main.cpp
@@ -6,12 +6,11 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/09 10:22:58 by fras          #+#    #+#                 */
-/*   Updated: 2024/05/11 23:12:07 by fras          ########   odam.nl         */
+/*   Updated: 2024/05/12 08:58:24 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Sed.hpp"
-#include <string>
 
 int	main(int argc, char *argv[])
 {
